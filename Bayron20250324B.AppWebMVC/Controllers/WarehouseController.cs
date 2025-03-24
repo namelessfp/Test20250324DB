6ff834b6702b74b92ecf89d1946da6254f3b7457
ALUMNOS <ALUMNOS@M04-CII>
2025-03-24 08:13:59 -0600

    creacion de los controlodaores

---
+++ Bayron20250324B.AppWebMVC/Controllers/WarehouseController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Bayron20250324B.AppWebMVC.Models;
+
+namespace Bayron20250324B.AppWebMVC.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        private readonly Test20250324DbContext _context;
+
+        public WarehouseController(Test20250324DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Warehouse
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Warehouses.ToListAsync());
+        }
+
+        // GET: Warehouse/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warehouse = await _context.Warehouses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+
+            return View(warehouse);
+        }
+
+        // GET: Warehouse/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Warehouse/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,WarehouseName,Notes")] Warehouse warehouse)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(warehouse);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(warehouse);
+        }
+
+        // GET: Warehouse/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warehouse = await _context.Warehouses.FindAsync(id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+            return View(warehouse);
+        }
+
+        // POST: Warehouse/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,WarehouseName,Notes")] Warehouse warehouse)
+        {
+            if (id != warehouse.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(warehouse);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WarehouseExists(warehouse.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(warehouse);
+        }
+
+        // GET: Warehouse/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warehouse = await _context.Warehouses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+
+            return View(warehouse);
+        }
+
+        // POST: Warehouse/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var warehouse = await _context.Warehouses.FindAsync(id);
+            if (warehouse != null)
+            {
+                _context.Warehouses.Remove(warehouse);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WarehouseExists(int id)
+        {
+            return _context.Warehouses.Any(e => e.Id == id);
+        }
+    }
+}
--- Bayron20250324B.AppWebMVC/Program.cs
+++ Bayron20250324B.AppWebMVC/Program.cs
@@ -1,7 +1,14 @@
+using Bayron20250324B.AppWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Test20250324DbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
 
 var app = builder.Build();
 
